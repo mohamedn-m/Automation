5f8227b110d2e23220cfbbe652d0a62522dfe33c
mohamedn-m <rihanfaris777@gmail.com>
2024-11-07 18:21:30 +0530

    Update HomePageTest.java

--- src/test/java/com/nn/testcase/homepage/HomePageTest.java
+++ src/test/java/com/nn/testcase/homepage/HomePageTest.java
@@ -19,25 +19,25 @@ public class HomePageTest extends BaseTest {
     }
 
 
-    @Test(priority = 2,dataProvider = "siteMap_Url",description = "verify the more than one H1 tag in the URL ")
+    //@Test(priority = 2,dataProvider = "siteMap_Url",description = "verify the more than one H1 tag in the URL ")
     public void H1TagChecker(String input) throws IOException, GeneralSecurityException {
         DriverActions.openURL(input);
         DriverActions.waitForTitleContains(("XML Sitemap"));
         verifyH1Tags();
     }
 
-    @Test(priority = 3,dataProvider = "siteMap_Url",description = "Verify images are mentioned in alt tag")
+   // @Test(priority = 3,dataProvider = "siteMap_Url",description = "Verify images are mentioned in alt tag")
     public void imageAltTagChecker(String input) throws GeneralSecurityException, IOException {
         DriverActions.openURL(input);
         DriverActions.waitForTitleContains(("XML Sitemap"));
         verifyImageAltAttributes();
     }
-    @Test(priority = 4,dataProvider = "siteMap_Url",description = "gffdgd")
+  //  @Test(priority = 4,dataProvider = "siteMap_Url",description = "gffdgd")
     public void verifyMetaData(String input) throws IOException, GeneralSecurityException {
         DriverActions.openURL(input);
         metaDataCheck();
     }
-    @Test(priority = 5,dataProvider = "siteMap_Url",description = "gffdgd")
+  //  @Test(priority = 5,dataProvider = "siteMap_Url",description = "gffdgd")
     public void Canonicalckeck(String input) throws IOException, GeneralSecurityException {
         DriverActions.openURL(input);
         DriverActions.waitForTitleContains(("XML Sitemap"));
@@ -46,7 +46,7 @@ public class HomePageTest extends BaseTest {
 
     @DataProvider()
     public Object[][] siteMap_Url(){
-        return new Object[][] {{"https://www.novalnet.de/post-sitemap.xml"},
+        return new Object[][] {/*{"https://www.novalnet.de/post-sitemap.xml"},
                 {"https://www.novalnet.de/page-sitemap.xml"},
                 {"https://www.novalnet.de/karriere-sitemap.xml"},
                 {"https://www.novalnet.de/integration-sitemap.xml"},
@@ -55,7 +55,7 @@ public class HomePageTest extends BaseTest {
                 {"https://www.novalnet.de/services-sitemap.xml"},
                 {"https://www.novalnet.de/mainp-sitemap.xml"},
                 {"https://www.novalnet.de/category-sitemap.xml"},
-                {"https://www.novalnet.de/post_tag-sitemap.xml"},
+                {"https://www.novalnet.de/post_tag-sitemap.xml"},*/
                 {"https://www.novalnet.com/glossary_categories-sitemap.xml"}};
     }
 
