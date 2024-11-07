31969248e4d6df7de47acb84fbb27ad6464687e1
mohamedn-m <rihanfaris777@gmail.com>
2024-11-07 18:07:16 +0530

    Update Test.java

--- src/test/java/com/nn/testcase/homepage/Test.java
+++ src/test/java/com/nn/testcase/homepage/Test.java
@@ -2,13 +2,16 @@ package com.nn.testcase.homepage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test {
 
     @org.testng.annotations.Test
    public void test(){
-
-        WebDriver driver = new ChromeDriver();
-       System.out.println("Hello world");
+System.out.println("Hello world");
+        options.addArguments("--headless");  // Run Chrome in headless mode
+        options.addArguments("--no-sandbox");  // Required in CI environments
+        options.addArguments("--disable-dev-shm-usage");  // Prevents issues with limited memory
+       
    }
 }
