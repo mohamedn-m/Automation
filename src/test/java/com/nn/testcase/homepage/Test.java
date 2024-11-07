2083b80a4b4a89d285a1bb4eb3d1e75e1c095d86
mohamedn-m <rihanfaris777@gmail.com>
2024-11-07 17:30:45 +0530

    Update Test.java

--- src/test/java/com/nn/testcase/homepage/Test.java
+++ src/test/java/com/nn/testcase/homepage/Test.java
@@ -1,8 +1,14 @@
 package com.nn.testcase.homepage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 
     @org.testng.annotations.Test
    public void test(){
+
+        WebDriver driver = new ChromeDriver();
        System.out.println("Hello world");
    }
 }
