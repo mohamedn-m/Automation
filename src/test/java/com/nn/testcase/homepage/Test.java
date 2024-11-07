f6aa77b548b0e977be6fc9a5ffee0da9c7f60a69
mohamedn-m <rihanfaris777@gmail.com>
2024-11-07 16:20:43 +0530

    Update Test.java

--- src/test/java/com/nn/testcase/homepage/Test.java
+++ src/test/java/com/nn/testcase/homepage/Test.java
@@ -1,3 +1,8 @@
-package com.nn.testcase.homepage;public class Test {
-  //test
+package com.nn.testcase.homepage;
+public class Test {
+
+    @org.testng.annotations.Test
+   public void test(){
+       System.out.println("Hello world");
+   }
 }
