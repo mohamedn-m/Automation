908d4f4e5a8fdefc898dbd39ce07313d3fb6578f
mohamedn_m <mohamedn_m@novalnetsolutions.com>
2024-11-07 16:17:50 +0530

    test

--- src/test/java/com/nn/testcase/homepage/Test.java
+++ src/test/java/com/nn/testcase/homepage/Test.java
@@ -1,2 +1,8 @@
-package com.nn.testcase.homepage;public class Test {
+package com.nn.testcase.homepage;
+public class Test {
+
+    @org.testng.annotations.Test
+   public void test(){
+       System.out.println("Hello world");
+   }
 }
